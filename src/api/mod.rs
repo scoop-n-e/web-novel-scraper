3457e3045de30c627c47c4e9bc0091451541f5e1
scoop-n-e <codinggonbe@gmail.com>
2025-08-21 23:44:40 +0900

    APIを叩く機能の既存実装を破棄

--- src/api/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod narou;
-pub mod nocturne;
--- src/api/nocturne/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod client;
-pub mod models;
-pub mod novel_api;
-
-pub use client::NocturneApiClient;
-pub use models::*;
-pub use novel_api::{NocturneNovelApi, NocturneSearchParams};
